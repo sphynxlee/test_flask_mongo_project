29af4878099cb56607b7fd7e80b2886211734fa4
Haidun Li <kaiton.ri@gmail.com>
2024-05-30 13:59:31 -0300

    chore: Refactor upload_data function And Remove JWT

--- app.py
+++ app.py
@@ -1,27 +1,11 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 import datetime
-import jwt
-# import requests
 
 app = Flask(__name__)
 
 client = MongoClient('mongodb://db:27017/')
 db = client['mydatabase']
-
-APP_CLIENT_ID = "your-tenat-id"
-TENANT_ID = "your-tenant-id"
-
-def verify_token(token):
-    # Microsoft public key endpoint
-    jwks_uri = "https://login.microsoftonline.com/common/discovery/v2.0/keys"
-    jwks = requests.get(jwks_uri).json()
-    public_keys = {jwk['kid']: jwt.algorithms.RSAAlgorithm.from_jwk(jwt.json.dumps(jwk)) for jwk in jwks['keys']}
-    kid = jwt.get_unverified_header(token)['kid']
-    public_key = public_keys[kid]
-    # decode and verify the token
-    decoded = jwt.decode(token, public_key, algorithms=['RS256'], audience='{APP_CLIENT_ID}', issuer='https://login.microsoftonline.com/{TENANT_ID}/v2.0')
-    return decoded
 
 @app.route('/')
 def home():
@@ -29,10 +13,7 @@ def home():
 
 @app.route('/upload', methods=['POST'])
 def upload_data():
-    # Assumes Bearer token
-    # token = request.headers.get('Authorization').split()[1]
     try:
-        # decoded = verify_token(token)
         data = request.json
         user_name = data.get('UserName')
         user_email = data.get('UserEmail')
@@ -49,8 +30,11 @@ def upload_data():
 
 @app.route('/uploads', methods=['GET'])
 def get_uploads():
-    uploads = list(db.uploads.find({}, {'_id': 0}))
-    return jsonify(uploads), 200
+    try:
+        uploads = list(db.uploads.find({}, {'_id': 0}))  # Excluding the _id field from the result
+        return jsonify(uploads), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
